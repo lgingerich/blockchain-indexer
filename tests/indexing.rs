9fbc3a7e26a1c798d171c14fba95298266a3fbb6
Landon Gingerich <landon.ging@gmail.com>
2025-02-25 15:43:11 -0600

    test: add abstract and sophon

--- tests/indexing.rs
+++ tests/indexing.rs
@@ -34,18 +34,46 @@ const ZKSYNC_PARAMS: [(u64, usize, usize, usize, usize); 4] = [
     (12464133, 1, 41, 250, 3893), // First block with a type 254 (0xfe: 254) transaction
 ];
 
+//////// Abstract test params ////////
+const ABSTRACT_RPC_URL: &str = "https://api.mainnet.abs.xyz";
+// ABSTRACT_PARAMS has the block number to process and the expected output row count for each dataset
+const ABSTRACT_PARAMS: [(u64, usize, usize, usize, usize); 5] = [
+    // (block_number, output_block_count, output_transaction_count, output_log_count, output_trace_count)
+    (3, 1, 1, 5, 35), // First block with a priority (0xff: 255) transaction
+    (6, 1, 1, 6, 92), // First block with an EIP-712 (0x71: 113)
+    (8, 1, 1, 4, 66), // First block with an EIP-1559 (0x2: 2) transaction
+    (9, 1, 1, 3, 61), // First block with a legacy (0x0: 0) transaction
+    (165866, 1, 9, 93, 1084), // First block with a type 254 (0xfe: 254) transaction
+];
+
+//////// Sophon test params ////////
+const SOPHON_RPC_URL: &str = "https://rpc.sophon.xyz";
+// SOPHON_PARAMS has the block number to process and the expected output row count for each dataset
+const SOPHON_PARAMS: [(u64, usize, usize, usize, usize); 5] = [
+    // (block_number, output_block_count, output_transaction_count, output_log_count, output_trace_count)
+    (3, 1, 1, 3, 35), // First block with a priority (0xff: 255) transaction
+    (4, 1, 1, 3, 61), // First block with an EIP-1559 (0x2: 2) transaction
+    (6, 1, 1, 7, 99), // First block with an EIP-712 (0x71: 113)
+    (42, 1, 1, 4, 63), // First block with a legacy (0x0: 0) transaction
+    (4044460, 1, 1, 27, 198), // First block with a type 254 (0xfe: 254) transaction
+];
+
 #[tokio::test]
 async fn test_indexing_pipeline() -> Result<()> {
     // Test blocks for each chain
     let test_cases = vec![
         // Ethereum blocks
-        (Chain::Ethereum, ETH_RPC_URL, ETH_PARAMS),
+        (Chain::Ethereum, "Ethereum", ETH_RPC_URL, ETH_PARAMS.to_vec()),
         // ZKSync blocks
-        (Chain::ZKsync, ZKSYNC_RPC_URL, ZKSYNC_PARAMS),
+        (Chain::ZKsync, "ZKsync Era", ZKSYNC_RPC_URL, ZKSYNC_PARAMS.to_vec()),
+        // Abstract blocks
+        (Chain::ZKsync, "Abstract", ABSTRACT_RPC_URL, ABSTRACT_PARAMS.to_vec()),
+        // Sophon blocks
+        (Chain::ZKsync, "Sophon", SOPHON_RPC_URL, SOPHON_PARAMS.to_vec()),
     ];
 
-    for (chain, rpc_url, block_cases) in test_cases {
-        println!("\nTesting {:?} chain", chain);
+    for (chain, chain_name, rpc_url, block_cases) in test_cases {
+        println!("\nTesting {} chain", chain_name);
 
         // Set up provider
         let provider = ProviderBuilder::new()
